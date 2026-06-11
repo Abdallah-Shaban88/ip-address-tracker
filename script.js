e556a9e18230bef504c8a3bfa6fc747c587825ae
unknown <abdallahshabaan88@yahoo.com>
2026-06-11 20:45:17 +0300

    use vite for .env

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
-
-window.addEventListener("load", function() {
-    var viewport = document.querySelector("meta[name=viewport]");
-    viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
-    
-})
-    var map = L.map('map', {
-    center: [0, -0],
-    zoom: 1,
-    
+window.addEventListener("load", function () {
+  var viewport = document.querySelector("meta[name=viewport]");
+  viewport.setAttribute(
+    "content",
+    viewport.content + ", height=" + window.innerHeight,
+  );
+});
+var map = L.map("map", {
+  center: [0, -0],
+  zoom: 1,
 });
 
-var layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-    layer.addTo(map)
+var layer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+layer.addTo(map);
 
-localStorage.setItem('map', map)
+localStorage.setItem("map", map);
 
- const searchInput = document.querySelector('input'),
-      searchForm = document.querySelector('.search-erea'),
-      ipInfoList = document.querySelector('.info-list'),
-      loader = document.querySelector('.loader')
-      
- let ip
- 
+const searchInput = document.querySelector("input"),
+  searchForm = document.querySelector(".search-erea"),
+  ipInfoList = document.querySelector(".info-list"),
+  loader = document.querySelector(".loader");
 
-searchForm.addEventListener('submit', (e)  =>{
+let ip;
+
+searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  ip = searchInput.value //&& searchInput.value !== ''
-  if(searchInput.value == ''){
-    console.log('empty value');
-    return
+  ip = searchInput.value; //&& searchInput.value !== ''
+  if (searchInput.value == "") {
+    console.log("empty value");
+    return;
   }
-   loader.style.display = 'block';
-   getData(ip)
-  searchInput.value = ''
-} )
+  loader.style.display = "block";
+  getData(ip);
+  searchInput.value = "";
+});
 
-let  getData = async (ip) => {
- let response = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_NbhUfB5HwTkC1euPGEJTL3FGhUSoX&domain=${ip}`)
- let data = response.json()
- .then(info => {
-   if(!data){
-     console.log("error")
-   }else{
-   console.log(info);
-   loader.style.display = 'none'
-   ipInfoList.innerHTML =`
+let getData = async (ip) => {
+  let response = await fetch(`${import.meta.env.VITE_apiKey}${ip}`);
+  let data = response.json().then((info) => {
+    if (!data) {
+      console.log("error");
+    } else {
+      console.log(info);
+      loader.style.display = "none";
+      ipInfoList.innerHTML = `
    <li>
     <span>IP Address</span>
     <span>${info.ip}</span>
@@ -62,11 +60,8 @@ let  getData = async (ip) => {
     <span>${info.isp}</span>
    </li>
    `;
-   L.marker([info.location.lat, info.location.lng]).addTo(map);
-   map.setView([info.location.lat, info.location.lng], 17)
- }
- })
-}
-
-
-
+      L.marker([info.location.lat, info.location.lng]).addTo(map);
+      map.setView([info.location.lat, info.location.lng], 17);
+    }
+  });
+};
